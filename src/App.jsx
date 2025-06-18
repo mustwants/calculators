b25cfd34f8313188b862d256cbaf035b40c55236
MustWants <90696663+mustwants@users.noreply.github.com>
2025-06-18 08:57:48 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,13 @@
+import React from 'react';
+import MortgageCalculator from './MortgageCalculator';
+
+function App() {
+  return (
+    <div>
+      <MortgageCalculator />
+    </div>
+  );
+}
+
+export default App;
 
